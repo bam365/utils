fabdc2f4ad8812f39319131c450907a24b3dc78a
Mitchell <bmitchell@Blakes-MacBook-Pro.local>
2014-04-29 11:41:14 -0500

    block mode now default, added support for emmet-style tag spec

--- ocaml/tag/tag.ml
+++ ocaml/tag/tag.ml
@@ -1,6 +1,60 @@
 open Core.Std
 
 
+module Tag: sig
+    type t
+
+    val from_str: string -> t option
+    val begin_str: t -> string
+    val end_str: t -> string
+
+end = struct
+    type t = {
+        tag_name: string;
+        id: string option;
+        clss: string option;
+    }
+
+    let tag_str_is_valid s =
+        let char_count c = String.count s ~f:((=) c) in
+        (char_count '.') <= 1 && (char_count '#') <= 1 
+
+    let get_tag_name s = 
+        match String.split_on_chars s ~on:['#'; '.'] with
+        | tag_name::_ -> tag_name
+        | _ -> "" (* should never happen *)
+
+    let get_attribute s first_char last_char =
+        match String.split s ~on:first_char with
+        | _::first_str::_ ->
+                (match String.split first_str ~on:last_char with
+                | str::_ -> Some str
+                | _ -> Some first_str)
+        | _ -> None
+
+
+    let from_str s =
+        if tag_str_is_valid s then
+            Some { tag_name = get_tag_name s; 
+                   id = get_attribute s '#' '.'; 
+                   clss = get_attribute s '.' '#';
+                 }
+        else None
+
+    let begin_str t =
+        let attr_str attr_name attr  = 
+            match attr with
+            | Some s -> sprintf " %s=\"%s\"" attr_name s
+            | None -> ""
+        in
+        sprintf "<%s%s%s>" t.tag_name (attr_str "id" t.id) 
+                           (attr_str "class" t.clss)
+
+    let end_str t = sprintf "</%s>" t.tag_name
+end
+    
+
+
 let trim_split str =
     let not_whitespace _ c = Char.is_whitespace c |> not in
     let str_len = String.length str in
@@ -25,16 +79,23 @@ let trim_split str =
 let add_tag_between_ws is_block tag str =
     let [left_ws; mid; right_ws] = trim_split str in
     let tag_sep = if is_block then "\n" ^ left_ws else "" in
-    sprintf "%s<%s>%s%s%s</%s>%s" 
+    sprintf "%s%s%s%s%s%s%s" 
         left_ws 
-        tag tag_sep mid tag_sep tag
+        (Tag.begin_str tag) tag_sep mid tag_sep (Tag.end_str tag)
         right_ws
 
-let main is_block tag () =
-    let transform = add_tag_between_ws in
-    In_channel.input_all In_channel.stdin
-    |> transform is_block (Option.value tag ~default:"")
-    |> Out_channel.output_string Out_channel.stdout
+let main is_inline tag_str () =
+    let tag_str = Option.value tag_str ~default:"" in 
+    match Tag.from_str tag_str with
+    | Some tag ->
+        let transform = add_tag_between_ws in
+        In_channel.input_all In_channel.stdin
+        |> transform (not is_inline) tag
+        |> Out_channel.output_string Out_channel.stdout
+    | None -> 
+        "Malformed tag"
+        |> Out_channel.output_string Out_channel.stderr;
+        Out_channel.output_string Out_channel.stdout tag_str
 
 
 (* Command stuff *)
@@ -42,7 +103,7 @@ let main is_block tag () =
 let spec =
     let open Command.Spec in
     empty 
-    +> flag "-b" no_arg ~doc:"block tag (put newlines after tags)"
+    +> flag "-i" no_arg ~doc:"inline tag (no newlines after tags)"
     +> anon (maybe ("tag" %: string))
 
 let command =
